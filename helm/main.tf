264fe12f9c4436c2a37056b913c8e3f15a73b1c9
praveenatadi27 <168502527+praveenatadi27@users.noreply.github.com>
2024-06-29 20:07:01 -0500

    Update main.tf

--- helm/main.tf
+++ helm/main.tf
@@ -42,56 +42,8 @@ output "module_path" {
   value = path.module
 }
 
-data "terraform_remote_state" "eks" {
-  backend = "remote"
-  config = {
-    # Update to your Terraform Cloud organization
-    organization = "praveena-tadi-org"
-    workspaces = {
-      name = "kubernetes-ops-staging-20-eks"
-    }
-  }
-}
-
 resource "aws_s3_bucket" "example" {
   bucket = "example-bucket-${terraform.workspace}"
   acl    = "private"
 }
 
-provider "helm" {
-  kubernetes {
-    host                   = data.terraform_remote_state.eks.outputs.cluster_endpoint
-    cluster_ca_certificate = base64decode(data.terraform_remote_state.eks.outputs.cluster_certificate_authority_data)
-    exec {
-      api_version = "client.authentication.k8s.io/v1alpha1"
-      args        = ["eks", "get-token", "--cluster-name", "${local.environment_name}"]
-      command     = "aws"
-    }
-  }
-}
-
-# Helm values file templating
-data "template_file" "helm_values" {
-  template = file("${path.module}/helm_values.yml")
-  # Parameters you want to pass into the helm_values.yaml.tpl file to be templated
-  vars = {
-    fullnameOverride = "${var.namespace}"
-    repository       = var.repository
-    tag              = var.tag
-  }
-}
-module "sample-app" {
-  source = "github.com/ManagedKube/kubernetes-ops//terraform-modules/aws/helm/helm_generic?ref=v1.0.30"
-  # this is the helm repo add URL
-  repository = "https://helm-charts.managedkube.com"
-  # This is the helm repo add name
-  official_chart_name = "standard-application"
-  # This is what you want to name the chart when deploying
-  user_chart_name = var.fullnameOverride
-  # The helm chart version you want to use
-  helm_version = "1.0.11"
-  # The namespace you want to install the chart into - it will create the namespace if it doesnt exist
-  namespace = var.namespace
-  # The helm chart values file
-  helm_values = data.template_file.helm_values.rendered
-}
